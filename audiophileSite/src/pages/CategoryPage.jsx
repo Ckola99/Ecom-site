ced38565389015b4733485c645955120639ac6dd
Christopher Kola <122616101+Ckola99@users.noreply.github.com>
2024-08-29 11:13:18 +0200

    Update CategoryPage.jsx

--- audiophileSite/src/pages/CategoryPage.jsx
+++ audiophileSite/src/pages/CategoryPage.jsx
@@ -14,21 +14,27 @@ const CategoryPage = () => {
 	categoryItems = categoryItems.sort(
 		(a, b) => (b.new ? 1 : -1) - (a.new ? 1 : -1)
 	);
-	console.log(categoryItems);
 
 	const format = (productName) => {
-		// Split the slug by dashes
 		const parts = productName.split(" ");
-		parts.pop();
-		const name = parts.join(" ");
-
-		return name;
+		parts.pop(); // Remove the last part (e.g., "headphones")
+		return parts.join(" ");
 	};
 
-	const [screenSize, setScreenSize] = useState(window.innerWidth >= 768 ? "tablet" : window.innerWidth >= 1024 ? "desktop" : "mobile" );
+	const [screenSize, setScreenSize] = useState(
+		window.innerWidth >= 1024 ? "desktop" : window.innerWidth >= 768 ? "tablet" : "mobile"
+	);
 
 	useEffect(() => {
 		const handleResize = () => {
+			if (window.innerWidth >= 1024) {
+				setScreenSize("desktop");
+			} else if (window.innerWidth >= 768) {
+				setScreenSize("tablet");
+			} else {
+				setScreenSize("mobile");
+			}
+		};
 
 		window.addEventListener("resize", handleResize);
 
@@ -38,7 +44,7 @@ const CategoryPage = () => {
 	}, []);
 
 	return (
-		<div className="">
+		<div>
 			<Helmet>
 				<title>{`${category} - Product Category`}</title>
 				<meta
@@ -60,31 +66,22 @@ const CategoryPage = () => {
 					<img
 						src={
 							screenSize === "tablet"
-								? item
-										.categoryImage
-										.tablet
-								: screenSize ===
-								  "desktop"
-								? item
-										.categoryImage
-										.desktop
-								: item
-										.categoryImage
-										.mobile
+								? item.categoryImage.tablet
+								: screenSize === "desktop"
+								? item.categoryImage.desktop
+								: item.categoryImage.mobile
 						}
 						alt={item.name}
 						className={`rounded-lg h-[352px] md:w-[689px] lg:h-full ${
-							index % 2 !== 0 &&
-							"lg:col-start-2"
-						} `}
+							index % 2 !== 0 && "lg:col-start-2"
+						}`}
 					/>
 					<div
 						className={`flex flex-col items-center w-[327px] h-auto gap-5 justify-between md:w-[572px] ${
 							!item.new && "mt-5"
 						} lg:items-start ${
-							index % 2 !== 0 &&
-							"lg:col-start-1 lg:row-start-1"
-						} `}
+							index % 2 !== 0 && "lg:col-start-1 lg:row-start-1"
+						}`}
 					>
 						{item.new && (
 							<p className="uppercase text-main-orange tracking-[10px] text-[14px] mt-5">
@@ -93,16 +90,12 @@ const CategoryPage = () => {
 						)}
 						<h2 className="uppercase text-[28px] font-bold tracking-[1px] md:text-[40px] text-center lg:text-left">
 							{format(item.name)}
-							<span className="block">
-								{item.category}
-							</span>
+							<span className="block">{item.category}</span>
 						</h2>
 						<p className="font-medium leading-[25px] text-center text-[15px] opacity-50 lg:text-left lg:w-[80%]">
 							{item.description}
 						</p>
-						<Link
-							to={`/product/${item.slug}`}
-						>
+						<Link to={`/product/${item.slug}`}>
 							<button className="w-[160px] h-[48px] bg-main-orange text-white uppercase text-[13px] font-bold tracking-[1px] hover:bg-[#FBAF85]">
 								see product
 							</button>
